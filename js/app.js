bba6a565e8f9d38166274cfc943c7770bb89b68d
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-11 16:03:03 -0400

    Basic Requirements Met
    Added named functions.  Now needs hints, such as "hotter" and "colder"

--- js/app.js
+++ js/app.js
@@ -16,10 +16,39 @@ $(document).ready(function(event){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	// valid inputs
 
-  	var answer = Math.floor(Math.random() * 100) + 1;
-  	var count = 0;
+  	function newNumber() {
+  		answer = Math.floor(Math.random() * 100) + 1;
+  	}
+
+  	function newGame() {
+  		newNumber();
+	  	count = 0;
+  		$("#feedback").text("Make your Guess");
+		$("#count").text(0);
+		$("#guessList").empty();
+
+  	}
+
+  	function feedback(a) {
+		if(a >= 50) {
+			$("#feedback").text("Ice Cold");
+		} else if ((50 > a) && (a >= 30)) {
+			$("#feedback").text("Cold");
+		} else if ((30 > a) && (a >= 20)) {
+			$("#feedback").text("Warm");
+		} else if ((20 > a) && (a >= 10)) {
+			$("#feedback").text("Hot");
+		} else if ((10 > a) && (a > 0)) {
+			$("#feedback").text("Very Hot");
+		} else if (a === 0) {
+			$("#feedback").text("Correct!  Congratulations!");
+		}
+  	}
+
+
+  	newGame();
+
 
   	$("#guessButton").on("click",
   		function(event) {
@@ -27,34 +56,15 @@ $(document).ready(function(event){
   			event.preventDefault();
 
   			var guess = $("#userGuess").val();
-  			// guess = +guess;
-  			// var answer = 25;
-  			var difference = answer - guess;
-    		var avDif = Math.abs(difference);
+    		var dif = Math.abs(answer - guess);
+    		// var dif1 = dif;
 
   			if ((101 > guess >= 1) && (guess % 1 === 0)) {
-	   			
-	   			if(avDif >= 50) {
-	  				$("#feedback").text("Ice Cold");
-	  			} else if ((50 > avDif) && (avDif >= 30)) {
-	  				$("#feedback").text("Cold");
-	  			} else if ((30 > avDif) && (avDif >= 20)) {
-	  				$("#feedback").text("Warm");
-	  			} else if ((20 > avDif) && (avDif >= 10)) {
-	  				$("#feedback").text("Hot");
-	  			} else if ((10 > avDif) && (avDif > 0)) {
-	  				$("#feedback").text("Very Hot");
-	  			} else if (avDif === 0) {
-	  				$("#feedback").text("Correct!  Congratulations!");
-	  			}
-	  				
+	   			feedback(dif);
 	  			count++;
 				$("#count").text(count);
-
 				$("#guessList").append('<li>'+guess+'</li>');
-
 				$("#userGuess").val("");
-
   			} else {
   				alert("Please enter a whole number from 1 to 100");
   			}
@@ -63,15 +73,7 @@ $(document).ready(function(event){
   		}
   	);  	
 
-	$(".new").on("click", function() {
-
-		$("#feedback").text("Make your Guess");
-		$("#count").text(0);
-		$("#guessList").empty();
-		answer = Math.floor(Math.random() * 100) + 1;
-	  	count = 0;
-
-	});
+	$(".new").on("click", newGame);
 
   	// Use enter key
 
